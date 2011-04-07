038c8d639ddea0334a44894be158dc7b59d07feb
Ti <ti@d1.ru>
2011-04-07 15:56:45 +0600

    update jquery version to 1.5.2, add favicon, change doctype to html5

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!doctype html>
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta charset="utf-8">
 	<title>jQuery router plugin</title>
 	<link rel="stylesheet" type="text/css" media="all" href="style.css" />
 	<link rel="stylesheet" type="text/css" media="all" href="style.css" />
 	
+	<link rel="shortcut icon" href="favicon.ico">
+	
 	<!-- highlight.js -->
 	<link rel="stylesheet" title="Default" href="highlight.js/styles/default.css">
 	<script src="highlight.js/highlight.pack.js"></script>
+	
 	<script>
 	hljs.tabReplace = '    ';
 	hljs.initHighlightingOnLoad();
